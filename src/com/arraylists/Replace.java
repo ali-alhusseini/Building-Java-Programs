5f2d724418f1e931fecb7762231742081efa97f7
Ali <ciwanbuhar@gmail.com>
2020-07-16 01:37:54 -0300

    Created the replace method

--- src/com/arraylists/Replace.java
+++ src/com/arraylists/Replace.java
@@ -1,4 +1,17 @@
 package com.arraylists;
 
+import java.util.ArrayList;
+
 public class Replace {
+    public static void main(String[] args) {
+        ArrayList<String> list = new ArrayList<>();
+
+    }
+
+    public static void replace(ArrayList<String> list, String target, String replacement){
+        int index = list.indexOf(target);
+        if (index >= 0) {
+            list.set(index, replacement);
+        }
+    }
 }
